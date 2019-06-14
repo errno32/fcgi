8ab59f15de2a45e6c7c853b55ce844a11df5ece8
Andrzej <mojsza.racja@gmail.com>
2019-06-14 18:14:52 +0200

    recording module

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "conf.h"
+
+#include "src/rec/record.h"
+
 
 int main(void)
 {
-	printf("Hello World!\n");
+	record rec;
+	REC("Rozpoczęcie pracy programu.%s", "");
 
+	REC("Wszystko oki, panie %s!", "Janie");
+	REC_ERR(OK, 32, "Złamana rura, hihi %s", ":-)");
+	REC_ERR(1, 2, "ojej, to je %d numer", 12);
+
+	REC("Zakończenie pracy programu.%s", "");
 	return 0;
 }	
+
 
